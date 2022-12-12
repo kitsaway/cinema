fe3a4783f337306407d12fbc9fd4ac914eb212ee
mtokhiani <tokhiiani@gmail.com>
2022-12-12 17:21:08 +0400

    added routes and user provider

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
 import Header from "./components/Header";
 import Movies from "./components/Movies";
 import Favourites from "./components/Favourites";
+import SignIn from './components/SignIn/index';
+import SignUp from './components/SignUp/index';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route index path="/" element={<Movies />}></Route>
-        <Route exact path="/favourites" element={<Favourites />}></Route>
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index path="/" element={<Movies />}></Route>
+          <Route exact path="/favourites" element={<Favourites />}></Route>
+          <Route exact path="/signin" element={<SignIn />}></Route>
+          <Route exact path="/signup" element={<SignUp />}></Route>
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 };
 

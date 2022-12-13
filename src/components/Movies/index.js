86106f7eda9e08ead903bd8e9b1cf31bbe318e0b
mtokhiani <tokhiiani@gmail.com>
2022-12-13 21:04:30 +0400

    minor changes

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useContext, useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
+import { Container, Typography } from "@mui/material";
 import MovieContext from "./../../context/MovieContext";
 import Movie from "../Movie";
 import AppPagination from "../Pagination";
@@ -25,18 +26,29 @@ const Movies = () => {
 
   return (
     <>
-      <Grid
-        container
-        spacing={{ xs: 2, md: 3 }}
-        columns={{ xs: 2, sm: 8, md: 12 }}
-      >
-        {currentMovies.map((movie) => (
-          <Grid item xs={2} sm={4} md={4} key={movie.id}>
-            <Movie movie={movie} />
+      {currentMovies.length > 0 ? (
+        <>
+          <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 2, sm: 8, md: 12 }}
+          >
+            {currentMovies.map((movie) => (
+              <Grid item xs={2} sm={4} md={4} key={movie.id}>
+                <Movie movie={movie} />
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
-      <AppPagination handlePageClick={handlePageClick} pageCount={pageCount} />
+          <AppPagination
+            handlePageClick={handlePageClick}
+            pageCount={pageCount}
+          />
+        </>
+      ) : (
+        <Typography sx={{ color: "#909090", textAlign: "center" }}>
+          No Movies Found
+        </Typography>
+      )}
     </>
   );
 };
